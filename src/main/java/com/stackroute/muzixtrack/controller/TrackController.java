5f9b6ee06ed7f974363953e00eb4c2c6433fb633
valasruthi <valasruthi4@mail.com>
2019-08-02 09:22:06 +0530

    updated track

--- src/main/java/com/stackroute/muzixtrack/controller/TrackController.java
+++ src/main/java/com/stackroute/muzixtrack/controller/TrackController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -20,24 +21,35 @@ public class TrackController {
    this.trackService=trackService;
   }
   @PostMapping("track")
+  //to post the track
   public ResponseEntity<?> saveTrack(@RequestBody Track track){
     Track savedTrack=trackService.save(track);
     return new ResponseEntity<>(savedTrack, HttpStatus.OK);
   }
 
   @GetMapping("track/{id}")
+  //to get the track by id
   public ResponseEntity<?> getTrackById(@PathVariable int id) {
     System.out.println(id);
     Track trackDetails=trackService.getById(id);
     return new ResponseEntity<>(trackDetails,HttpStatus.OK);
   }
  @GetMapping("track")
+ //to get the List of track
   public ResponseEntity<?> getAllTracks(){
    return new ResponseEntity<List<Track>>(trackService.getAllTracks(),HttpStatus.OK);
   }
-  /*@DeleteMapping("user/{id}")
-  public ResponseEntity<?> deleteUserById(@PathVariable("id") int id) {
-    List<Track> trackList = trackService.deleteTrackById(id);
-    return new ResponseEntity<>(trackList,HttpStatus.OK);
-  }*/
+  @DeleteMapping("track/{id}")
+  //to delete the track
+  public ResponseEntity<?> deleteTrackById(@PathVariable("id") int id) {
+    Optional<Track> tracksList = (Optional<Track>) trackService.deleteTrackById(id);
+    return new ResponseEntity<>(tracksList,HttpStatus.OK);
+  }
+  @PatchMapping("track/{id}")
+  //to update the track
+  public ResponseEntity<?> updateTrackById(@PathVariable int id, @RequestBody Track track) {
+    Track trackUpdated = trackService.updateTrack(id, track);
+    return new ResponseEntity<>(trackUpdated, HttpStatus.ACCEPTED);
+  }
+
 }
--- src/main/java/com/stackroute/muzixtrack/service/TrackService.java
+++ src/main/java/com/stackroute/muzixtrack/service/TrackService.java
@@ -3,11 +3,12 @@ package com.stackroute.muzixtrack.service;
 import com.stackroute.muzixtrack.domain.Track;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TrackService {
   public Track save(Track track);
   public Track getById(int id);
   public List<Track> getAllTracks();
-   public Track deleteTrackById(int id);
-  public Track updateTrack(int id);
+   public Optional<Track> deleteTrackById(int id);
+ public Track updateTrack(int id,Track track);
 }
